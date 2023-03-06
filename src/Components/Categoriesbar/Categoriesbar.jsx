d3a4d0d2eb94d8079f75f8d796f0451990ebb10d
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-03-06 19:18:58 +0530

    categoriesbar Added

--- src/Components/Categoriesbar/Categoriesbar.jsx
+++ src/Components/Categoriesbar/Categoriesbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./_categories.scss";
 
+const keywords = [
+  'All',
+  'React js',
+  'Angular js',
+  'React Native',
+  'use of API',
+  'Redux',
+  'Music',
+  'Algorithm Art ',
+  'Guitar',
+  'Bengali Songs',
+  'Coding',
+  'Cricket',
+  'Football',
+  'Real Madrid',
+  'Gatsby',
+  'Poor Coder',
+  'Shwetabh',
+]
+
+
 const Categoriesbar = () => {
+
+  const [active , setActive] = useState("All");
+
+  const handleClick = (ele)=>{
+      setActive(ele);
+  }
   return (
-    <div>Categoriesbar</div>
+    <div className='Categoriesbar' >
+       {
+        keywords.map((ele,ind)=>{
+          return(
+            <span key={ind}
+            onClick={()=> handleClick(ele)}
+            className={active === ele ? "active":"" }
+             >{ele}
+             </span>  //top Categories bar
+          )
+        })
+       }
+    </div>
   )
 }
 
