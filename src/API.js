9f5e6529b0487833e460c1ceb4bd726e4c59f37d
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-03-13 03:17:24 +0530

    .env Fixed

--- src/API.js
+++ src/API.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-
+// import axios from "axios";
+console.log(process.env)
 const request = axios.create({
     baseURL:'https://youtube.googleapis.com/youtube/v3/',
     params: {
-        // key : `${process.env.REACT_APP_YOU_TUBE_API_KEY}`,
-        key:'AIzaSyCzbeDcbtAPu1NR54YtOcsGE_0Clh8ikGo',
+        key : `${process.env.REACT_APP_YOU_TUBE_API_KEY}`,
     }
+    
 })
 
 export default request;
