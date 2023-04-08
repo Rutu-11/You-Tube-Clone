2ab6153d0bf2c708fe24004601a8cdd422ecef94
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-04-08 20:06:17 +0530

    logo1

--- src/Components/VideoWatchSideBar/VideoWatchSidebar.jsx
+++ src/Components/VideoWatchSideBar/VideoWatchSidebar.jsx
@@ -9,7 +9,7 @@ import { Col, Row } from "react-bootstrap";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function VideoWatchSidebar({ video, searchScreen }) {
+function VideoWatchSidebar({ video, searchScreen, subScreen }) {
   const {
     id,
     snippet: {
@@ -85,50 +85,53 @@ function VideoWatchSidebar({ video, searchScreen }) {
   const thumbnail = !isVideo && "videoHorizontal__thumbnail-channel";
   return (
     <Row
-      className="py-2 m-1 videoHorizontal align-items-center"
-      onClick={handleClick}
-    >
-      {/* //!conditionsl rendering of search-screen and realatedVideos-screen */}
-      <Col xs={6} md={searchScreen ? 4 : 6} className="videoHorizontal__left">
-        <LazyLoadImage
+    className='py-2 m-1 videoHorizontal align-items-center'
+    onClick={handleClick}>
+    {/* //TODO refractor grid */}
+    <Col
+       xs={6}
+       md={searchScreen || subScreen ? 4 : 6}
+       className='videoHorizontal__left'>
+       <LazyLoadImage
           src={medium.url}
-          effect="blur"
+          effect='blur'
           className={`videoHorizontal__thumbnail ${thumbnail} `}
-          wrapperClassName="videoHorizontal__thumbnail-wrapper"
-        />
+          wrapperClassName='videoHorizontal__thumbnail-wrapper'
+       />
+       {isVideo && (
+          <span className='videoHorizontal__duration'>{_duration}</span>
+       )}
+    </Col>
+    <Col
+       xs={6}
+       md={searchScreen || subScreen ? 8 : 6}
+       className='p-0 videoHorizontal__right'>
+       <p className='mb-1 videoHorizontal__title'>{title}</p>
 
-        {isVideo && (
-          <span className="videoHorizontal__duration">{_duration}</span>
-        )}
-      </Col>
-
-      <Col
-        xs={6}
-        md={searchScreen ? 8 : 6}
-        className="videoHorizontal__right p-0"
-      >
-        <p className="mb-1 videoHorizontal__title">{title}</p>
-
-        {isVideo && (
-          <div className=" videoHorizontal__details">
-            {/* //append number of views */}
-            <AiFillEye /> {numeral(views).format("0.a")} Views •
-            {/* To render the time after publication */}
-            {moment(publishedAt).fromNow()}
+       {isVideo && (
+          <div className='videoHorizontal__details'>
+             <AiFillEye /> {numeral(views).format('0.a')} Views •
+             {moment(publishedAt).fromNow()}
           </div>
-        )}
+       )}
 
-        {isVideo && (
-          <p className="mt-1 ">{description} </p>
-        )}
+       {(searchScreen || subScreen) && (
+          <p className='mt-1 videoHorizontal__desc'>{description}</p>
+       )}
 
-        <div className="my-1 videoHorizontal__channel d-flex align-items-center">
-          {isVideo && <LazyLoadImage src={channelIcon?.url} effect="blur" />}
-
-          <p className="mb-0">{channelTitle}</p>
-        </div>
-      </Col>
-    </Row>
+       <div className='my-1 videoHorizontal__channel d-flex align-items-center'>
+          {isVideo && (
+             <LazyLoadImage src={channelIcon?.url} effect='blur' />
+          )}
+          <p className='mb-0'>{channelTitle}</p>
+       </div>
+       {subScreen && (
+          <p className='mt-2'>
+             {video.contentDetails.totalItemCount} Videos
+          </p>
+       )}
+    </Col>
+ </Row>
   );
 }
 
