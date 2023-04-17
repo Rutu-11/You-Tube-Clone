f436ea4314225dfcc4138a0e93cc72b85765efb3
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-04-17 18:09:12 +0530

    c

--- src/API.js
+++ src/API.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const request = axios.create({
     baseURL:'https://youtube.googleapis.com/youtube/v3/',
     params: {
-        key : `${process.env.REACT_APP_YOU_TUBE_API_KEY}`,
-       
+        // key : `${process.env.REACT_APP_YOU_TUBE_API_KEY}`,
+        key:"AIzaSyDxwaaLMbi7I98CBFRU08glUMJJmtOsXwg"
     
     }
     
