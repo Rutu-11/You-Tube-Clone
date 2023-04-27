650faffddd718d57f0f400ee626d82d700aae800
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-04-27 16:39:36 +0530

    redirects added2

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 console.log('key',process.env.REACT_APP_YOU_TUBE_API_KEY)
-const key = process.env.REACT_APP_YOU_TUBE_API_KEY;
+// const key = process.env.REACT_APP_YOU_TUBE_API_KEY;
 const request = axios.create({
     baseURL:'https://youtube.googleapis.com/youtube/v3/',
     params: {
