417a301ba5365abbab3be4df516256a7f6c7699f
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-04-27 16:45:01 +0530

    redirects added3

--- src/API.js
+++ src/API.js
@@ -6,7 +6,7 @@ const request = axios.create({
     baseURL:'https://youtube.googleapis.com/youtube/v3/',
     params: {
         key : `${process.env.REACT_APP_YOU_TUBE_API_KEY}`,
-
+       
     }
     
 })
